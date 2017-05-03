aa83646ffbd67ab0380190ea6dbfede01d25cec3
Ender187 <m.hiltawski@hotmail.com>
2017-05-03 18:22:48 +0200

    zadanie wykonane

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 	add.addEventListener('click', function() {
-		list.innerHTML += '<li>item </li>';
-
+		list.innerHTML += '<li>item ' +document.getElementsByTagName('li').length;
 });
 
